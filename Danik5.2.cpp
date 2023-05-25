68b115c5e2da7143f1853a7155924b8e82bf5c5c
OceJI4455E <134645122+OceJI4455E@users.noreply.github.com>
2023-05-25 23:14:51 +0300

    Add files via upload

---
+++ Danik5.2.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+using namespace std;
+
+struct People {
+    string surname;
+    int numberOfpoints;
+    int occupiedPlace;
+public:
+    void print() {
+        cout << "Фамилия: " << surname << "\tКоличество очков: " << numberOfpoints << "\tЗанятое место: " << occupiedPlace << endl;
+    }
+};
+
+const int HASH_TABLE_SIZE = 15;
+
+class HashTable {
+private:
+    vector<People> table;
+    int hash1(int key) {
+        return key % HASH_TABLE_SIZE;
+    }
+    int hash2(int key) {
+        return (key / HASH_TABLE_SIZE) % HASH_TABLE_SIZE;
+    }
+public:
+    HashTable() {
+        table.resize(HASH_TABLE_SIZE);
+    }
+    void insert(People people) {
+        int index = hash1(people.numberOfpoints);
+        int step = hash2(people.occupiedPlace);
+        while (!table[index].surname.empty()) {
+            index = (index + step) % HASH_TABLE_SIZE;
+        }
+        table[index] = people;
+    }
+    People* search(int key) {
+        int index = hash1(key);
+        int step = hash2(key);
+        while (!table[index].surname.empty() && table[index].numberOfpoints != key) {
+            index = (index + step) % HASH_TABLE_SIZE;
+        }
+        if (table[index].surname.empty()) {
+            return nullptr;
+        }
+        else {
+            return &table[index];
+        }
+    }
+    void print() {
+        for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+            if (table[i].surname != "")
+                cout << "[" << i << "]\t->\t" << "Фамилия: " << table[i].surname << "\tКоличество очков: " << table[i].numberOfpoints << "\tЗанятое место: " << table[i].occupiedPlace << endl;
+            else
+                cout << "[" << i << "]\t->\t" << "Нет данных\n";
+        }
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(time(NULL));
+
+    const int n = 5;
+    People people[n];
+    for (int i = 0; i < n; i++) {
+        system("cls");
+        cout << "Ввод данных о человеке No " << i + 1 << endl;
+        cout << "Введите фамилию: ";
+        cin >> people[i].surname;
+        people[i].numberOfpoints = rand() % 200;
+        people[i].occupiedPlace = rand() % (1 - 9);
+
+        //для ручного ввода
+        /*cout << "Ввод данных о человеке No " << i + 1 << endl;
+        cout << "Введите фамилию: ";
+        cin >> people[i].surname;
+        cout << "Введите Количество очков: ";
+        cin >> people[i].numberOfpoints;
+        cout << "Введите Занятое место: ";
+        cin >> people[i].occupiedPlace;*/
+    }
+
+    HashTable table;
+    for (int i = 0; i < n; i++) {
+        table.insert(people[i]);
+    }
+
+    cout << "Исходный массив:" << endl;
+    for (int i = 0; i < n; i++) {
+        people[i].print();
+    }
+
+    cout << "Хеш-таблица:" << endl;
+    table.print();
+    system("pause");
+
+    char key;
+    do
+    {
+        system("cls");
+        table.print();
+        cout << "-------------------------------------------------\n";
+        cout << "Введите ключ для поиска (Вес): ";
+        int searchObject;
+        cin >> searchObject;
+
+        People* result = table.search(searchObject);
+        if (result == nullptr) {
+            cout << "Элемент не найден" << endl;
+        }
+        else {
+            cout << "Найденный элемент: ";
+            result->print();
+        }
+        cout << "-------------------------------------------------\n";
+        cout << "Продолжить поиск? (да - любая клавиша, нет - 1): ";
+        cin >> key;
+    } while (key != '1');
+
+
+    return 0;
+}
---
+++ Danik5.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+
+const int HASH_TABLE_SIZE = 15;
+const int MIN_RANGE = 38000;
+const int MAX_RANGE = 58000;
+
+int hashFunction(int key);
+void printArray(std::vector<int> arr);
+void printHashTable(std::vector<int> hashTable);
+void searchInHashTable(std::vector<int> hashTable);
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    int n = 9;
+    std::vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % (MAX_RANGE - MIN_RANGE) + MIN_RANGE;
+    }
+
+    std::vector<int> hashTable(HASH_TABLE_SIZE, -1);
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= MIN_RANGE && arr[i] <= MAX_RANGE) {
+            int index = hashFunction(arr[i]);
+            while (hashTable[index] != -1) {
+                index = (index + 1) % HASH_TABLE_SIZE;
+            }
+            hashTable[index] = arr[i];
+        }
+    }
+
+    printArray(arr);
+    std::cout << "----------------------------------------------------------------------------------------------------------\n";
+    printHashTable(hashTable);
+    system("pause");
+
+    char key;
+    do
+    {
+        system("cls");
+        printHashTable(hashTable);
+        std::cout << "----------------------------------------------\n";
+        searchInHashTable(hashTable);
+        std::cout << "----------------------------------------------\n";
+        std::cout << "Хотите продолжить? (нет - 0, да - любая клавиша)\n";
+
+        std::cin >> key;
+    } while (key != '0');
+
+
+    return 0;
+}
+
+int hashFunction(int key) {
+    return key % HASH_TABLE_SIZE;
+}
+void printArray(std::vector<int> arr)
+{
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
+}
+void printHashTable(std::vector<int> hashTable)
+{
+    std::cout << "Хеш-таблица:\n";
+    for (int i = 0; i < hashTable.size(); i++) {
+        std::cout << "[" << i + 1 << "]\t->\t" << hashTable[i] << '\n';
+    }
+    std::cout << '\n';
+}
+void searchInHashTable(std::vector<int> hashTable)
+{
+    int searchKey;
+    std::cout << "Введите ключ для поиска: ";
+    std::cin >> searchKey;
+
+    int index = hashFunction(searchKey);
+    while (hashTable[index] != searchKey && hashTable[index] != -1) {
+        index = (index + 1) % HASH_TABLE_SIZE;
+    }
+
+    if (hashTable[index] == searchKey) {
+        std::cout << "Элемент НАЙДЕН в хеш-таблице\n";
+    }
+    else {
+        std::cout << "Элемент НЕ НАЙДЕН в хеш-таблице\n";
+    }
+}
